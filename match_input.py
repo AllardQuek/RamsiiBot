590a3f2b81114c6e9b4b3990446ac0700ea65f03
James Lao <Jlprime@users.noreply.github.com>
2021-01-08 13:19:42 +0800

    Update match_input.py

--- match_input.py
+++ match_input.py
@@ -5,12 +5,28 @@ engine = create_engine('sqlite:///ingredient_list.db')  # accesses ingredient li
 
 def userSearch(user_input):
     with engine.connect() as connection:
-        result = connection.execute("select ing_input from ingredient_substitution")
+        result = connection.execute("select * from ingredient_substitution")
         searchFailure = True
+        cs = ""
+        sp = ""
+        alt = ""
         for row in result:
-            if user_input.lower() == str(row['ing_input']):
-                print("Success")
+            if user_input.lower() == row['ing_input']:				#searches each row of the db to access the relevant data
                 searchFailure = False
+                if row['ing_cs'] == "NIL":
+                    cs = ""
+                else:
+                    cs = "\n" + row['ing_cs']
+                if row['ing_sp'] == "NIL":
+                    sp = ""
+                else:
+                    sp = "\n" + row['ing_sp']
+                if row['ing_alt'] == "NIL":
+                    alt = ""
+                else:
+                    alt = "\n" + row['ing_alt']
+                print(row['full_ing'] + cs + sp + alt)
+
                 break
         if searchFailure:
             print("Ingredient cannot be found! Please try another one.")
