81dcedb2c7f1e8d37bb7b48a88cb59493fabd3be
James Lao <Jlprime@users.noreply.github.com>
2021-01-08 11:17:00 +0800

    optimization 1
    help_command

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 from pprint import pprint
 import os
 import spoonacular as sp
-
+from help_command import help_command
 
 api_instance = sp.API("7622a72decf948a0b1fb094128e2f884")
 
@@ -47,12 +47,6 @@ def start(update: Update, context: CallbackContext) -> None:
     username = update.message.chat.username
 
     update.message.reply_text(f"Hi {username}! I'm Ramsay, the best chef in the world with 7 Michelin stars!")              
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
