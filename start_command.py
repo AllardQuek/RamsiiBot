57073c503eacce81dadc1f163b15e6c00dce29ac
Allard <AllardQuek@users.noreply.github.com>
2021-01-08 11:45:29 +0800

    formatted response

--- start_command.py
+++ start_command.py
@@ -4,11 +4,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     username = update.message.chat.username
-
-    intro = f"""
-        Hi {username}! I'm Ramsay, the best chef in the world with 7 Michelin stars! 
-        Missing an ingredient for your recipe? Let me find you a substitute right away!
-        Or if you're bored, try /trivia to get a random trivia!
-    """
+    intro = f"Hi {username}! I'm Ramsay, the best chef in the world with 7 Michelin stars! Missing an ingredient for your recipe? Let me find you a substitute right away! Or if you're bored, try /trivia to get a random trivia!"
 
     update.message.reply_text(intro)              
